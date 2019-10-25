017a65720eb56a5b51dc06dc9d37241c566e15f2
Алан Агнаев <agnaevagnaev@outlook.com>
2019-10-25 18:12:16 +0300

    Json deserializer

---
+++ JsonDeserializer.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp
+{
+
+    internal static class Helper
+    {
+        public static IEnumerable<JToken> AllChildren(JToken json)
+        {
+            foreach (var c in json.Children())
+            {
+                yield return c;
+                foreach (var cc in AllChildren(c))
+                {
+                    yield return cc;
+                }
+            }
+        }
+        public async static Task<string> GetContentAsync()
+        {
+            string result = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                result = await client.GetStringAsync(GetLink(0, 1));
+            }
+            return result;
+        }
+
+        public async static Task<string> GetContentAsync(int offset)
+        {
+            string result = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                result = await client.GetStringAsync(GetLink(offset));
+            }
+            return result;
+        }
+
+        public async static Task<string> GetContentAsync(int offset, int limit)
+        {
+            string result = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                result = await client.GetStringAsync(GetLink(offset, limit));
+            }
+            return result;
+        }
+
+        public async static Task<string> GetContentAsync(string link)
+        {
+            string result = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                result = await client.GetStringAsync(link);
+            }
+            return result;
+        }
+
+        public static string GetLink(int offset, int limit = 100)
+        {
+            return $"http://opendata.trudvsem.ru/api/v1/vacancies/region/15?offset={offset}&limit={limit}";
+        }
+    }
+    public class JsonDeserializer
+    {
+        public static void LocalMain()
+        {
+            List<vacancy> vacancies = new List<vacancy>();
+            int countOfVacancies = JsonConvert.DeserializeObject<Responce>(Task.Run(async () => await Helper.GetContentAsync(0)).Result).meta.total;
+            int countOfPages = countOfVacancies / 100 + 1;
+
+            for (int offset = 0; offset < countOfPages; offset++)
+            {
+                string json = Task.Run(async () => await Helper.GetContentAsync(offset)).Result;
+                var resultObjects = Helper.AllChildren(JObject.Parse(json))
+                    .First(c => c.Type == JTokenType.Array && c.Path.Contains("vacancies"))
+                    .Children<JObject>();
+
+                foreach (JObject result in resultObjects)
+                {
+                    foreach (JProperty property in result.Properties())
+                    {
+                        vacancy vacancy = JsonConvert.DeserializeObject<vacancy>(property.Value.ToString());
+                        vacancies.Add(vacancy);
+                        Console.WriteLine(property.Name + " " + property.Value);
+                    }
+                }
+            }
+            Console.WriteLine($"{countOfPages}\n{countOfVacancies}");
+        }
+        private class Responce
+        {
+            public string status { get; set; }
+            public request request { get; set; }
+            public meta meta { get; set; }
+            [JsonProperty("results")]
+            public results results { get; set; }
+        }
+
+        public class request
+        {
+            public string api { get; set; }
+        }
+
+        public class meta
+        {
+            public int total { get; set; }
+            public int limit { get; set; }
+        }
+
+        public class results
+        {
+            [JsonProperty("vacancies")]
+            public List<vacancy> vacancies { get; set; }
+        }
+
+        
+        public class vacancy
+        {
+            [JsonProperty("id")]
+            public Guid id { get; set; }
+            [JsonProperty("source")]
+            public string source { get; set; }
+            [JsonProperty("region")]
+            public region region { get; set; }
+            [JsonProperty("company")]
+            public company company { get; set; }
+            [JsonProperty("creation-date")]
+            public DateTime creation_date { get; set; }
+            [JsonProperty("modify-date")]
+            public DateTime modify_date { get; set; }
+            [JsonProperty("salary")]
+            public string salary { get; set; }
+            [JsonProperty("salary_min")]
+            public int salary_min { get; set; }
+            [JsonProperty("salary_max")]
+            public int salary_max { get; set; }
+            [JsonProperty("job-name")]
+            public string job_name { get; set; }
+            [JsonProperty("vac_url")]
+            public string vac_url { get; set; }
+            [JsonProperty("employment")]
+            public string employment { get; set; }
+            [JsonProperty("schedule")]
+            public string schedule { get; set; }
+            [JsonProperty("duty")]
+            public string duty { get; set; }
+            public category category { get; set; }
+            [JsonProperty("requirement")]
+            public requirement requirement { get; set; }
+            [JsonProperty("addresses")]
+            public addresses addresses { get; set; }
+            [JsonProperty("currency")]
+            public string currency { get; set; }
+        }
+
+        public class region
+        {
+            public string region_code { get; set; }
+            public string name { get; set; }
+        }
+
+        public class company
+        {
+            public string ogrn { get; set; }
+            public string site { get; set; }
+            public string companycode { get; set; }
+            [JsonProperty("hr-agency")]
+            public bool hr_agency { get; set; }
+            public string inn { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string url { get; set; }
+        }
+
+        public class category
+        {
+            public string specialisation { get; set; }
+        }
+
+        public class requirement
+        {
+            public string qualification { get; set; }
+            public string experience { get; set; }
+        }
+
+        public class addresses
+        {
+            public List<address> address { get; set; }
+        }
+
+        public class address
+        {
+            public string location { get; set; }
+            public string lng { get; set; }
+            public string lat { get; set; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,130 +17,16 @@ using System.Runtime.Serialization;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Runtime;
-
+using System.Web.Script.Serialization;
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
-        public static string json = @"
-			{
-  				""success"": true,
-  				""message"":
-                [
-                    {
-                        status: ""OK""
-                    },
-                    {
-                        status: ""Lol""
-                    }
-                ],
-			  	""types"": 
-			  	[
-					{
-				  		""name"": ""A5EF3-ASR"",
-				  		""title"": ""ITIL Foundation Plus Cloud Introduction"",
-				  		""classroomDeliveryMethod"": ""Self-paced Virtual Class"",
-				  		""descriptions"": {
-							""EN"": {
-					  			""description"": ""some Text null"",
-					  			""overview"": null,
-					  			""abstract"": ""Some other text"",
-					  			""prerequisits"": null,
-					  			""objective"": null,
-					  			""topic"": null
-							}
-				  		},
-				  		""lastModified"": ""2014-10-08T08:37:43Z"",
-				  		""created"": ""2014-04-28T11:23:12Z""
-					},
-					{
-				  		""name"": ""A4DT3-ASR"",
-				  		""title"": ""ITIL Foundation eLearning Course + Exam"",
-				  		""classroomDeliveryMethod"": ""Self-paced Virtual Class"",
-				  		""descriptions"": {
-							""EN"": {
-					  			""description"": ""some Text"",
-					  			""overview"": null,
-					  			""abstract"": ""abstract test"",
-					  			""prerequisits"": null,
-					  			""objective"": null,
-					  			""topic"": null
-							}
-				  		},
-				  		""lastModified"": ""2014-10-09T08:37:43Z"",
-				  		""created"": ""2014-04-29T11:23:12Z""
-					}
-				]
-			}";
         static void Main(string[] args)
         {
-            //Myscheduler.intervalinseconds(17, 40, 10, () =>
-            //{
-            //    console.writeline("hello");
-            //});
-
-            //var responce = new RequestTo("http://opendata.trudvsem.ru/api/v1/vacancies/region/15?offset=0&limit=1").GetResponse()
-            //    .Replace("creation-date", "creation_date").Replace("modify-date", "modify_date").Replace("job-name", "job_name")
-            //    .Replace("hr-agency", "hr_agency");
-            //Responce result = JsonConvert.DeserializeObject<Responce>(responce);
-
-
-            //using (WebClient client = new WebClient())
-            //{
-            //    string rawJson = client.DownloadString(Helper.GetLink(0, 1));
-            //    Responce vacancies = JsonConvert.DeserializeObject<Responce>(rawJson);
-            //    Console.WriteLine(vacancies);
-            //}
-
-            string json = Task.Run(async () => await Helper.getcontentasync()).Result;
-            int offset = JsonConvert.DeserializeObject<JsonDeserialize.Responce>(json).meta.total / 100 + 1;
-            List<JsonDeserialize.vacancy_container> vacancies = new List<JsonDeserialize.vacancy_container>();
-            for(int page = 1; page <= offset; page++)
-            {
-                var resultObjects = AllChildren(JObject.Parse(json))
-                    .First(c => c.Type == JTokenType.Array && c.Path.Contains("vacancies"))
-                    .Children<JObject>();
-
-                foreach (JObject result in resultObjects)
-                {
-                    //foreach (JProperty property in result.Properties())
-                    //{
-                    string currentVacancy = result.ToString()
-                        .Replace("creation-date", "creation_date").Replace("modify-date", "modify_date").Replace("job-name", "job_name")
-                        .Replace("hr-agency", "hr_agency"); ;//.Insert(0, "{") + "}";
-                        JsonDeserialize.vacancy_container vac = JsonConvert.DeserializeObject<JsonDeserialize.vacancy_container>(currentVacancy);
-                        //JsonDeserialize.vacancy vac = JsonSerializer.
-                        vacancies.Add(vac);
-                    //}
-                }
-                json = Task.Run(async () => await Helper.getcontentasync(page)).Result;
-            }
-            Console.WriteLine(offset + " " + JsonConvert.DeserializeObject<JsonDeserialize.Responce>(json).meta.total);
+            JsonDeserializer.LocalMain();
             Console.ReadKey();
         }
-        private static IEnumerable<JToken> AllChildren(JToken json)
-        {
-            foreach (var c in json.Children())
-            {
-                yield return c;
-                foreach (var cc in AllChildren(c))
-                {
-                    yield return cc;
-                }
-            }
-        }
-    }
-
-    static class Helper
-    {
-        public async static Task<string> getcontentasync() => 
-            await new HttpClient().GetStringAsync(GetLink(0));
-
-        public async static Task<string> getcontentasync(int offset) =>
-            await new HttpClient().GetStringAsync(GetLink(offset));
-
-        public static string GetLink(int offset, int limit = 100) =>
-            $"http://opendata.trudvsem.ru/api/v1/vacancies/region/15?offset={offset}&limit={limit}";
     }
 }
